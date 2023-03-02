f498c4e30c2cffe4a7bf3933a859d3e7262f8d9c
BT <bahartofigh@gmail.com>
2023-03-02 18:30:53 +0330

    remove .live from checkoutToken in Review component.

--- src/components/CheckoutFolder/Review.jsx
+++ src/components/CheckoutFolder/Review.jsx
@@ -1,60 +1,59 @@
 import React from 'react';
 import { Typography , List , ListItem , ListItemText } from '@material-ui/core';
 
-// const Review = ({ckeckoutToken}) => {
-//   return (
-//     <>
-//     <Typography variant="h6" gutterBottom>خلاصه سفارش</Typography>
-//     <List disablePadding>
-//       {ckeckoutToken.live.line_items.map((product) =>(
-//         <ListItem style={{padding:'10px 0'}} key={product.name}>
-//           <ListItemText primary={product.name} secondary={`Quantity: ${product.quantity}`} />
-//             <Typography variant='body2'>
-//               {product.line_total.formatted_with_symbol}
-//             </Typography>
-//         </ListItem>
-//       ))}
-//       <ListItem style={{padding:'10px 0'}}>
-//           <ListItemText primary="Total" />
-//             <Typography variant='subtitle1' style={{fontWeight: 700}}>
-//               {ckeckoutToken.live.subtotal.formatted_with_symbol}
-//             </Typography>
-//         </ListItem>
-//     </List>
-//     </>
-//   )
-// }
-
-// export default Review
-const Review = ({checkoutToken}) => {
+const Review = ({ checkoutToken }) => {
   return (
     <>
-        <Typography variant="h6" gutterBottom>خلاصه سفارش</Typography>
-        <List disablePadding>
-        <ListItem>
-        <ListItemText primary="ساعت sonto" secondary="تعداد: 2 عدد"/>
-        <Typography variant='body2'>
-            85$
+    <Typography variant="h6" gutterBottom>خلاصه سفارش</Typography>
+    <List disablePadding>
+      {checkoutToken.line_items.map((product) => (
+        <ListItem style={{ padding: '10px 0' }} key={product.name}>
+          <ListItemText primary={product.name} secondary={`تعداد محصول: ${product.quantity}`} />
+          <Typography variant="body2">{product.line_total.formatted_with_symbol}</Typography>
+        </ListItem>
+      ))}
+      <ListItem style={{ padding: '10px 0' }}>
+        <ListItemText primary="جمع کل :" />
+        <Typography variant="subtitle1" style={{ fontWeight: 700 }}>
+          {checkoutToken.subtotal.formatted_with_symbol}
         </Typography>
-        </ListItem>
-            {/* {checkoutToken.live.line_items.map((product) =>(
-        <ListItem style={{padding:'10px 0'}} key={product.name}>
-            <ListItemText primary={product.name} secondary={`تعداد: ${product.quantity}`} />
-            <Typography variant='body2'>
-                {product.line_total.formatted_with_symbol}
-            </Typography>
-        </ListItem>
-    ))} */}
-       <ListItem style={{padding:'10px 0'}}>
-           <ListItemText primary="قیمت نهایی" />
-             <Typography variant='subtitle1' style={{fontWeight: 700}}>
-               {/* {checkoutToken.live.subtotal.formatted_with_symbol} */}
-               115$
-             </Typography>
-         </ListItem>
-        </List>
+      </ListItem>
+    </List>
     </>
- )
+  )
 }
 
 export default Review
+
+// const Review = ({checkoutToken}) => {
+//   return (
+//     <>
+//         <Typography variant="h6" gutterBottom>خلاصه سفارش</Typography>
+//         <List disablePadding>
+//         <ListItem>
+//         <ListItemText primary="ساعت sonto" secondary="تعداد: 2 عدد"/>
+//         <Typography variant='body2'>
+//             85$
+//         </Typography>
+//         </ListItem>
+            {/* {checkoutToken.live.line_items.map((product) =>(
+//         <ListItem style={{padding:'10px 0'}} key={product.name}>
+//             <ListItemText primary={product.name} secondary={`تعداد: ${product.quantity}`} />
+//             <Typography variant='body2'>
+//                 {product.line_total.formatted_with_symbol}
+//             </Typography>
+//         </ListItem>
+//     ))} */}
+//        <ListItem style={{padding:'10px 0'}}>
+//            <ListItemText primary="قیمت نهایی" />
+//              <Typography variant='subtitle1' style={{fontWeight: 700}}>
+//                {/* {checkoutToken.live.subtotal.formatted_with_symbol} */}
+//                115$
+//              </Typography>
+//          </ListItem>
+//         </List>
+//     </>
+//  )
+// }
+
+// export default Review
