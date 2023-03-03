eee26f8ff577eb15c5d391bd44f9bfe50b33de63
BT <bahartofigh@gmail.com>
2023-03-03 22:52:09 +0330

    remove rewrite codes in Product component.

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -31,33 +31,4 @@ const Product = ({ product, onAddToCart }) => {
   )
 }
 
-export default Product
-
-// const Product = ({ product, onAddToCart }) =>  {
-//     const classes = useStyles();
-
-//     return(
-//         <Card className={classes.root}>
-//         <CardMedia className={classes.media} image={product.image.url} title={product.name}/>
-//             <CardContent>
-//             <div className={classes.cardContent}>
-//                     <Typography variant='h5' gutterBottom>
-//                     {product.name}
-//                     </Typography>
-//                     <Typography variant='h5' >
-//                     {product.price.formatted_with_symbol}
-//                     </Typography>
-//             </div>
-//                 <Typography  dangerouslySetInnerHTML={{__html: product.description }}  variant='body2' color='textSecondary'/>
-//             </CardContent>
-//         <CardActions disableSpacing className={classes.cardActions}>
-//         <IconButton aria-label="Add to Cart" onClick={() => onAddToCart(product.id, 1)}>
-//                 <AddShoppingCart/>
-//             </IconButton>
-//         </CardActions>
-//     </Card>
-//     )
-
-// }
-
-// export default Product
+export default Product;
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -7,11 +7,6 @@ import Product from './Product/Product';
 // STYLES
 import useStyles from './styles';
 
-// const products = [
-//     {id:1 , name:'کفش' , description: 'مناسب برای دویدن' , price:'55$'},
-//     {id:2 , name:'کیف' , description: 'جا دار ' , price:'76$'}
-// ]
-
 const Products = ({products , onAddToCart }) => {
     const classes = useStyles();
     console.log(products);
@@ -32,26 +27,4 @@ const Products = ({products , onAddToCart }) => {
   )
 }
 
-export default Products
-
-
-// const Products = ({products , onAddToCart }) => {
-//     const classes = useStyles();
-
-//     return(
-//         <main className={classes.content}>
-//             <div className={classes.toolbar} />
-//         <Grid container justifyContent="center" spacing={4}>
-//         {
-//                 products.map((product) => (
-//                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-//                         <Product product={product} onAddToCart={onAddToCart}/>
-//                     </Grid>
-//                 ))
-//             }
-//         </Grid>
-//         </main>
-
-//     )
-// }
-// export default Products;
+export default Products;
