ed88cb74bc684dedf2be7a5a19e624924e837411
BT <bahartofigh@gmail.com>
2023-03-17 22:08:42 +0330

    remove old codes in CartItem folder.

--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -27,28 +27,4 @@ const CartItem = ({item , handleUpdateCartQuantity , onRemoveFromCart}) => {
   )
 }
 
-export default CartItem
-
-
-// const CartItem = ({item , handleUpdateCartQuantity , onRemoveFromCart}) => {
-//     const classes = useStyles();
-//   return (
-//     <Card>
-//         <CardMedia image={item.image.url} alt={item.name} className={classes.media} />
-//         <CardContent className={classes.CardContent}>
-//                 <Typography variant="h4">{item.name}</Typography>
-//                 <Typography variant="h4">{item.line_total.formatted_with_symbol}</Typography>
-//         </CardContent>
-//         <CardActions className={classes.cardActions}>
-//                 <div className={classes.buttons}>
-//                 <Button type="button" size="small" onClick={() => handleUpdateCartQuantity (item.id, item.quantity - 1)}>-</Button>
-//                     <Typography>{item.quantity}</Typography>
-//                     <Button type="button" size="small" onClick={() => handleUpdateCartQuantity (item.id, item.quantity + 1)}>+</Button>
-//                 </div>
-//                 <Button varient="contained" type="button" color="primary" onClick={() => onRemoveFromCart(item.id)}>حذف</Button>
-//         </CardActions>
-//     </Card>
-//   )
-// }
-
-// export default CartItem;
+export default CartItem;
